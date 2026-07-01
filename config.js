76fd4a060191e58b577f5ca716abf05d508edf09
smiles343 <smileboosterscommunity@gmail.com>
2026-07-01 23:19:59 +0300

    Update OWNER_NUMBER default value in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   BOT_NAME: process.env.BOT_NAME || 'LYNX',
-  OWNER: process.env.OWNER_NUMBER || ''
+  OWNER: process.env.OWNER_NUMBER || '254797856232'
 };
